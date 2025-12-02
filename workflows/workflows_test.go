b1eb43d1b398bb232cf341e5433f0e7a6f89c8b4
Ernesto Alejo <ernestokarim@gmail.com>
2025-12-02 16:40:55 +0100

    Test interrupted workflows by errors that never recover.

--- workflows/workflows_test.go
+++ workflows/workflows_test.go
@@ -20,11 +20,11 @@ import (
 
 var waitCh = make(chan struct{})
 
-func waitWorkflow(t *testing.T) {
+func waitWorkflow(t *testing.T, timeout time.Duration) {
 	select {
 	case <-waitCh:
 		return
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeout):
 		require.Fail(t, "workflows: timed out")
 	}
 }
@@ -38,9 +38,11 @@ var queue = cloudtasks.NewQueue(
 	cloudtasks.WithServiceAccountEmail("cloudtasks-tests@altipla-dev.iam.gserviceaccount.com"),
 )
 
-func initTestbed(t *testing.T) {
+func init() {
 	telemetry.Configure(logging.Debug())
+}
 
+func initTestbed(t *testing.T) {
 	if os.Getenv("REMOTE_HOSTNAME") == "" {
 		t.Skip("skipping remote test without REMOTE_HOSTNAME env variable")
 	}
@@ -78,7 +80,7 @@ func TestSimple(t *testing.T) {
 	initTestbed(t)
 
 	require.NoError(t, simpleWorkflow.Start(context.Background(), queue, nil))
-	waitWorkflow(t)
+	waitWorkflow(t, 10*time.Second)
 }
 
 func TestSimpleWithName(t *testing.T) {
@@ -86,7 +88,7 @@ func TestSimpleWithName(t *testing.T) {
 
 	name := ksuid.New().String()
 	require.NoError(t, simpleWorkflow.Start(context.Background(), queue, "payload", workflows.WithName(name)))
-	waitWorkflow(t)
+	waitWorkflow(t, 10*time.Second)
 }
 
 var returnWorkflow = workflows.Define("return", func(run *workflows.Run[string]) error {
@@ -124,7 +126,7 @@ func TestReturn(t *testing.T) {
 	initTestbed(t)
 
 	require.NoError(t, returnWorkflow.Start(context.Background(), queue, "start-payload"))
-	waitWorkflow(t)
+	waitWorkflow(t, 10*time.Second)
 }
 
 func TestReturnWithName(t *testing.T) {
@@ -132,7 +134,7 @@ func TestReturnWithName(t *testing.T) {
 
 	name := ksuid.New().String()
 	require.NoError(t, returnWorkflow.Start(context.Background(), queue, "start-payload", workflows.WithName(name)))
-	waitWorkflow(t)
+	waitWorkflow(t, 10*time.Second)
 }
 
 var panicContinuesAfterRetries = workflows.Define("panic-continues-after-retries", func(run *workflows.Run[any]) error {
@@ -157,5 +159,26 @@ func TestPanicContinuesAfterRetries(t *testing.T) {
 	initTestbed(t)
 
 	require.NoError(t, panicContinuesAfterRetries.Start(context.Background(), queue, nil))
-	waitWorkflow(t)
+	waitWorkflow(t, 10*time.Second)
+}
+
+var interrupted = workflows.Define("interrupted", func(run *workflows.Run[any]) error {
+	workflows.Step(run, "step-a", func(ctx context.Context) error {
+		return errors.New("step error")
+	})
+
+	workflows.Step(run, "step-b", func(ctx context.Context) error {
+		slog.Error("never reached")
+		return nil
+	})
+
+	waitCh <- struct{}{}
+	return nil
+})
+
+func TestInterrupted(t *testing.T) {
+	initTestbed(t)
+
+	require.NoError(t, interrupted.Start(context.Background(), queue, nil))
+	waitWorkflow(t, 1*time.Minute)
 }
